9dda7f031d1c1ab00aa3f7d0bba6476e269ddfec
Liam <liameguia@gmail.com>
2022-10-03 20:19:52 +0100

    implemented a refresh shortcode function

---
+++ src/shortener/management/commands/refreshcodes.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from shortener.models import KirrURL
+
+class Command(BaseCommand):
+    help = 'Refreshes all KirrURL shortcodes'
+
+    # def add_arguments(self, parser):
+    #     parser.add_argument('poll_ids', nargs='+', type=int)
+    
+    def handle(self, *args, **options):
+        return KirrURL.objects.refresh_shortcodes()
